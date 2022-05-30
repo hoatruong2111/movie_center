47036b0f2b9ae22c6b33cf6d2573ef18f486f8aa
hoatruong2111 <105116545+hoatruong2111@users.noreply.github.com>
2022-05-30 23:58:47 +0700

    change

--- setup.sh
+++ setup.sh
@@ -2,3 +2,10 @@ export DATABASE_PATH='postgresql://postgres:123456789@localhost:5432/movies'
 export AUTH0_DOMAIN='dev-gggdeid6.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='movie-center'
+
+export CAN_GET_REVIEWS="get:reviews-detail"
+export CAN_POST_MOVIES="post:movies"
+export CAN_POST_REVIEWS="post:reviews"
+export CAN_PATCH_MOVIES="patch:movies"
+export CAN_DELETE_MOVIES="delete:movies"
+export CAN_DELETE_REVIEWS="delete:reviews"
